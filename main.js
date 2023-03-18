70902ead55177e37da1d6fb650de6b9331620b40
shinjo15 <shinjo0015@gmail.com>
2023-03-18 09:16:53 +0900

    edit header

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
-const trainer = document.querySelecter('.trainer__switching__t');
-const staff = document.querySelecter('.trainer__switching__s');
+// const trainer = document.querySelecter('.trainer__switching__t');
+// const staff = document.querySelecter('.trainer__switching__s');
 
-function toggleactive(el){
-    el.classList.toggle('active')
+// function toggleactive(el){
+//     el.classList.toggle('active')
+// }
+
+// trainer.addEventListener('click', toggleactive(this));
+// staff.addEventListener('click', toggleactive(this));
+
+
+const title = document.querySelector('.mv__title');
+const header = document.querySelector('.header');
+
+const headerBg = function(entries, observer){
+    entries.forEach(entry =>{
+        if(entry.isIntersecting){
+            header.classList.remove('h-scroll');
+            console.log("in");
+        } else{
+            header.classList.add('h-scroll');
+            console.log("out");
+        }
+    });
 }
 
-trainer.addEventListener('click', toggleactive(this));
-staff.addEventListener('click', toggleactive(this));
+const option = {
+    rootMargin: "-60px",
+}
+
+const io = new IntersectionObserver(headerBg, option);
+io.observe(title);
