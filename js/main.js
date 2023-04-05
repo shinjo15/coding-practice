7b3e60b59c9df6c0d69bd61c0969515e689e08ba
shinjo15 <shinjo0015@gmail.com>
2023-04-05 11:03:04 +0900

    edit audio bgParallax

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded',
+  function(){
+    const soundOff = document.querySelector('.sound-off');
+    const soundOn = document.querySelector('.sound-on');
+    const bgm = document.querySelector('.bgm');
+
+    function soundOffCb(){
+        soundOff.classList.remove('active');
+        soundOn.classList.add('active');
+        bgm.play();
+    }
+
+    function soundOnCb(){
+        soundOn.classList.remove('active');
+        soundOff.classList.add('active');
+        bgm.pause();
+    }
+
+    soundOff.addEventListener('click',soundOffCb);
+    soundOn.addEventListener('click',soundOnCb);
+  }
+);
